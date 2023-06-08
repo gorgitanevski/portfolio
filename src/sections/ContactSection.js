f0a5653f60a91e7a07259d416d6a7dbffa0e942a
gorgitanevski <gorgitanevski11@gmail.com>
2023-06-08 17:06:59 +0200

    netfly form

--- src/sections/ContactSection.js
+++ src/sections/ContactSection.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import ContactMap from "../components/ContactMap";
+import { NetlifyForm, NetlifyInput } from "react-netlify-form";
 
 const ContactSection = () => {
   const [loaded, setLoaded] = useState(false);
@@ -17,18 +18,21 @@ const ContactSection = () => {
       <div className="flex flex-col sm:max-w-[80%]">
         <h2 className="text-5xl text-white font-bold mb-7">Contact me</h2>
 
-        <form className="flex flex-col gap-5 xl:text-base text-xl ">
+        <form
+          data-netlify="true"
+          className="flex flex-col gap-5 xl:text-base text-xl "
+        >
           <div className="flex sm:flex-col gap-3">
             <input type="text" placeholder="Name" />
             <input type="email" placeholder="Email" />
           </div>
           <input type="text" placeholder="Subject" />
           <textarea placeholder="Message" />
-        </form>
 
-        <button className="border text-semibold px-6 py-1 bg-[#042C54] text-white text-xl transition duration-300 ease-in-out hover:bg-white hover:text-black mt-7 self-end">
-          Sent
-        </button>
+          <button className="border text-semibold px-6 py-1 bg-[#042C54] text-white text-xl transition duration-300 ease-in-out hover:bg-white hover:text-black mt-7 self-end">
+            Sent
+          </button>
+        </form>
       </div>
       <ContactMap />
     </section>
