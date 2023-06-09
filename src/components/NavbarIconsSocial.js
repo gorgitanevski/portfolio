e75a4caaa0c409ecb20c93d93a3bd5f1e20a3ea1
gorgitanevski <gorgitanevski11@gmail.com>
2023-06-09 17:25:40 +0200

    add social

--- src/components/NavbarIconsSocial.js
+++ src/components/NavbarIconsSocial.js
@@ -4,14 +4,18 @@ import {
   AiFillLinkedin,
   AiOutlineFacebook,
 } from "react-icons/ai";
+import { Link } from "react-router-dom";
 const NavbarIconsSocial = ({ additionalClass }) => {
   return (
     <div
       className={`text-xl text-white flex lg:flex-row flex-col  justify-center itmes-center  gap-2 ${additionalClass}`}
     >
-      <AiFillGithub />
-      <AiFillLinkedin />
-      <AiOutlineFacebook />
+      <Link to="https://github.com/gorgitanevski">
+        <AiFillGithub />
+      </Link>
+      <Link to="https://www.linkedin.com/in/gorgi-tanevski-99086a27a/">
+        <AiFillLinkedin />
+      </Link>
     </div>
   );
 };
